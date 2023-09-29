fc374d1922e14dddba38bf3aa555336eb752ad03
eeennsu <diso592@naver.com>
2023-09-29 09:38:29 +0900

    init(1)
    css파일 복붙 해야함

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import type { FC } from 'react';
+
+
+
+
+const App: FC = () => {
+
+
+
+    return (
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatem tempore debitis voluptatibus eaque? Nobis nam excepturi fugit sit praesentium officia saepe repudiandae placeat unde maiores nostrum cupiditate, quos modi architecto asperiores doloremque incidunt error, nihil aperiam! Quidem fuga impedit quos ut. Accusantium tempore nihil at laudantium nostrum rem perferendis?
+        </div>
+    );
+};
+
+export default App;
